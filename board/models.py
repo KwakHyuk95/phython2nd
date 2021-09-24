4ad0608801deb15ea40281b03d5aa63e8ba68a95
yjjjjjjjy <yunjy0088@gmail.com>
2021-09-24 09:59:25 +0900

    테스트 커밋

---
+++ board/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+# ORM
+# 데이터베이스의 데이터를 조회하거나 저장하기위해 원래는
+# 퀘리(SQL)을 사용했지만 Django에서는
+# 데이터베이스 테이블을 '모델화'해서 사용하고 쿼리들을
+# Django 기준 python 코드로 처리하는 방법.
+
+# ORM의 특징
+# 개발자만의 쿼리(SQL)을 만들기가 어렵다.
+# 쿼리를 잘못 작성할 가능성이 낮아진다(간단한 명령에 한해서)
+# DBMS를 변경(이관)할때 쿼리를 바꿀 필요가 없다.
+
+# 모델 작성
+# 질문, 답변 모델을 생성
+# 질문모델
+# subject : 질문의 제목
+# content : 질문의 내용
+# create_date : 질문을 작성한 일시
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200) # CharField 문자의 길이를 제한
+    content = models.TextField() # TextField 제한없이 문자를 입력받고 싶을때 씀
+    create_date = models.DateTimeField() # 날짜나 시간은 상황에 맞게 TextField나 DateTimeField 사용
+
+    def __str__(self):
+        return self.subject
+
+# 답변모델
+# question : 질문
+# content : 답변의 내용
+# create_date : e답변 작성 일시.
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # on_delete=models.CASCADE는 답변에 연결된 질문이 삭제되면 답변도 함께 삭제하라는 의미
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
---
+++ board/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Answer
+from django.utils import timezone
+# 경로를 못찾을때에는 앞에 마침표를 찍어 같은 경로라는걸을 알려준다.
+from django.http import HttpResponse
+# Create your views here.
+
+# 주석달기
+# 언제 누가 뭘 위해서 작성하는지
+# 메인 페이지 작성
+# 질문 목록 출력
+#21.09.17 곽혁 작성
+def index(request):
+    # Question.objects.order_by('-create_date')
+    # Question 모델에서 객체를 참조하는데 / -가 붙어있으면 해당 객체를 기준으로 역순정렬
+    # 역순으로 정렬해올것 (객체는 create_date)
+    question_list = Question.objects.order_by('-create_date')
+    context ={'question_list': question_list}
+    # 템플릿단에 던짐 / 변수에 담지않고 던져도 상관 X
+    return render(request, 'question_list.html', context) # render는 조회성
+
+def detail(request, question_id):
+    # 글의 제목과 내용 출력
+    question = Question.objects.get(id=question_id)
+    return render(request, 'question_detail.html', {'question': question})
+
+def test(request):
+    return render(request, 'index.html')
+
+# 21.09.23 답변 등록하기
+# 작성자: 곽혁
+# get_object_or_404 : 사용자에게 보여지는 에러 메세지 제어 함수
+#                     데이터의 유츌을 막기 위한 방법.
+# redirect : 페이지의 재 요청 데이터를 전송 후 페이지를 새로고침하기 위함
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # 다음과 같은 방식이 가능했던 이유는 question, answer 모델이 외래키로 연결되어 있기 때문
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # answer 모델을 직접 사용하는 방법.
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('board:detail', question_id=question.id)
