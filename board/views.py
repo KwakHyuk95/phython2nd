21a8d64109fc65b096462e9b800a885f5b67c548
yjjjjjjjy <yunjy0088@gmail.com>
2021-10-06 10:07:36 +0900

    2021.10.06

--- board/views.py
+++ board/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # 경로를 못찾을때에는 앞에 마침표를 찍어 같은 경로라는걸을 알려준다.
 from django.http import HttpResponse
 # Create your views here.
@@ -103,3 +104,57 @@ def question_create(request):
     else:
         form = QuestionForm()
     return render(request, 'board/question_form.html', {'form': form})
+
+# 21.10.05 곽혁 질문 수정 기능 추가.
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('board:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, 'board/question_form.html', {'form': form})
+
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # 질문을 한 사람과 글을 작성한 사람이 같은지를 확인.
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다.')
+        return redirect('board:detail', question_id=question.id)
+    question.delete()
+    return redirect('board:index')
+
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('board:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'board/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('board:detail', question_id=answer.question.id)
